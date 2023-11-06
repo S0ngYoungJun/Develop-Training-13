8193760e6dc700a2b209a41363f402f107fcfe7a
songyoungjun <clesaaaaaa@gmail.com>
2023-11-06 18:02:06 +0900

    내맘대로변경

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // built-in modules
-import fs from "fs";
+import fs from "fs"; // << 앤 대체 왜 여기 있음?
 
 // custom modules
 import diffLogic from "./model/diff-logic.js";
 
 // json files path
-const inputJSONPath = "./config/fromDB-data.json";
-const outputJSONPath = "./config/differences.json";
+const inputJSONPath = "./data/fromDB-data.json"; // << 앤 왜 config 폴더?
+const outputJSONPath = "./data/differences.json";
 
 // controller
 
